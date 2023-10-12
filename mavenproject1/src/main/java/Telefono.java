461d0180a4680773d615bee8accea8a3a884282b
httpp4t0 <httpp4t0@DESKTOP-POM2PEE>
2023-10-12 03:31:15 -0300

    Agrega clase abstracta producto

---
+++ mavenproject1/src/main/java/Telefono.java
@@ -0,0 +1,55 @@
+
+/**
+ *
+ * @author httpp4t0
+ */
+public class Telefono extends Producto {
+    
+    private String marca;
+    private String modelo;
+    
+    //Constructor sin parametros
+    public Telefono() {
+    }
+    
+    //Constructor parametros solo telefono
+    public Telefono(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+    
+    //Constructor con parametros + super
+    public Telefono(String nombre, int preciosinIVA, String marca, String modelo) {
+        super(nombre, preciosinIVA);
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Telefono{"+ super.toString() + "marca=" + marca + ", modelo=" + modelo + '}';
+    }
+    
+
+    @Override
+    public String descripcionDetallada() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
