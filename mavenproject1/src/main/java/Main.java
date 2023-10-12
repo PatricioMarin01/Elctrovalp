605f18c82d0fec0f9bc4440ded3d90785cecd164
httpp4t0 <httpp4t0@DESKTOP-POM2PEE>
2023-10-12 04:50:39 -0300

    a

--- mavenproject1/src/main/java/Main.java
+++ mavenproject1/src/main/java/Main.java
@@ -11,10 +11,19 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         
-        ArrayList<Producto> ListaProductos = new ArrayList<>();
+        ArrayList<Producto> listaProductos = new ArrayList<>();
                 
         Telefono iphone = new Telefono("Iphone x", 890000, "IOS", "x12345");
         Computador computador1 = new Computador("Intel", "Torre", "PC pepe", 600000);
+        
+        listaProductos.add(iphone);
+        listaProductos.add(computador1);
+        
+        for (Producto producto : listaProductos) {
+            System.out.println(producto.descripcionDetallada());
+            System.out.println("------------------------------");
+            
+        }
         
 
         
